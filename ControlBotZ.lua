272572624100215057b67e60b919307460890c3f
Cracked691000 <138363157+Cracked691000@users.noreply.github.com>
2025-10-27 14:51:02 +0800

    Update ControlBotZ.lua

--- ControlBotZ.lua
+++ ControlBotZ.lua
@@ -35,7 +35,7 @@ if LocalPLR.Name ~= Username then
     local logChat = getgenv().logChat
     webhook = getgenv().webhook
 
-    ! = getgenv().Prefix
+    "!" = getgenv().Prefix
 
     local bots = getgenv().Bots
 
